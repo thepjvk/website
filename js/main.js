e7f775cc04f0e7f20054e7fec8c507656845ffc7
AlexanderLaurencio <ramonalaurencio5@gmail.com>
2026-02-12 06:19:25 -0400

    Improving the scroll

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const buttonTheme = document.querySelector("#buttonTheme");
 const header = document.querySelector("#header");
+const headerMainWrapper = document.querySelector(".header-main__wrapper");
+const html = document.querySelector("#html");
+const section1 = document.getElementsByTagName("section")[0];
+const section2 = document.getElementsByTagName("section")[1];
 
-//THIS CODE IS FOR THE HEADER ANIMATIONS
+let browserWidth = window.innerWidth;
 
-//header.addEventListener("animationend", function(e){
-//    if (e.animationName === "top-in") {
-//        header.classList.replace("header__in","header__scroll");
-//    }
-//})
+if (browserWidth > 970){
+    html.classList.add("html-scroll-type");
+    headerMainWrapper.classList.add("scroll-align");
+    section1.classList.add("scroll-align");
+    section2.classList.add("scroll-align");
+} else {
+    console.log("Your screen is large!!!");
+}
